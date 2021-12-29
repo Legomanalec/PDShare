cda6e6478ff058f53c2d1ea8f1fffdedc877e9c6
Legomanalec <alecrmeyer4@gmail.com>
2021-12-29 10:54:20 -0600

    Added basic front-end back-end functionality

--- PDShare/App.js
+++ PDShare/App.js
@@ -1,52 +1,69 @@
 //npx react-native start
 //npx react-native run-ios
+//arch -x86_64 pod install
 
 //xed ios ##opens project in xcode
 
- import React from 'react';
- import type {Node} from 'react';
- import Pdf from 'react-native-pdf';
- 
- import { StyleSheet, Dimensions, View } from 'react-native';
- 
- 
- 
- const App: () => Node = () => {
-   const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
-   return (
-     <View style={styles.container}>
-       <Pdf
-         source={source}
-         onLoadComplete={(numberOfPages,filePath) => {
-             console.log(`Number of pages: ${numberOfPages}`);
-         }}
-         onPageChanged={(page,numberOfPages) => {
-             console.log(`Current page: ${page}`);
-         }}
-         onError={(error) => {
-             console.log(error);
-         }}
-         onPressLink={(uri) => {
-             console.log(`Link pressed: ${uri}`);
-         }}
-         style={styles.pdf}/>
-     </View>
-   );
- };
- 
- const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       justifyContent: 'flex-start',
-       alignItems: 'center',
-       marginTop: 25,
-   },
-   pdf: {
-       flex:1,
-       width:Dimensions.get('window').width,
-       height:Dimensions.get('window').height,
-   }
- });
- 
- export default App;
- 
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './src/screens/Home';
+
+// we will use these two screens later in the Navigator
+import AddItem from './src/screens/AddItem';
+import List from './src/screens/List';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AddItem" component={AddItem} />
+        <Stack.Screen name="List" component={List} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+
+
+
+// import 'react-native-gesture-handler';
+// import React, { useEffect, useState } from 'react'
+// import { NavigationContainer } from '@react-navigation/native'
+// import { createStackNavigator } from '@react-navigation/stack'
+// import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+// import {decode, encode} from 'base-64'
+// if (!global.btoa) {  global.btoa = encode }
+// if (!global.atob) { global.atob = decode }
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+
+//   const [loading, setLoading] = useState(true)
+//   const [user, setUser] = useState(null)
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         { user ? (
+//           <Stack.Screen name="Home">
+//             {props => <HomeScreen {...props} extraData={user} />}
+//           </Stack.Screen>
+//         ) : (
+//           <>
+//             <Stack.Screen name="Login" component={LoginScreen} />
+//             <Stack.Screen name="Registration" component={RegistrationScreen} />
+//           </>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
